d702fc4893f34ac9a34f2eb34f99307d77edf2b6
Muhammad Usman <ch.usman564@gmail.com>
2022-03-23 02:59:17 +0500

    Complete header section

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
-let btn = document.querySelector('.btn');
+let btn = document.querySelector(".btn");
 
-btn.addEventListener('click', function (e) {
-  document.querySelector('.header__navigation--background').classList.toggle('active-bg');
-  document.querySelector('.header__nav').classList.toggle('active-nav');
-  document.querySelector('.header__nav-list').classList.toggle('active-nav-list');
+btn.addEventListener("click", function (e) {
+  if (e.target.classList.contains("header__nav")) return;
+
+  document
+    .querySelector(".header__navigation--background")
+    .classList.toggle("active-bg");
+  document.querySelector(".header__nav").classList.toggle("active-nav");
+  document
+    .querySelector(".header__nav-list")
+    .classList.toggle("active-nav-list");
+  document
+    .querySelector(".header__navigation--icon")
+    .classList.toggle("icon-toggle");
 });
