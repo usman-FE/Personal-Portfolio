320dc0d15b53e7e36e3f2abd0c0235801ef9ff29
Usman-FE <ch.usman564@gmail.com>
2022-03-29 08:52:05 +0500

    Fix about parallax for medium screens

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 const btn = document.querySelector(".btn");
-const aboutParallex = document.getElementById('about');
-const aboutParallexImg = document.getElementById('about-img');
-const portImg1 = document.getElementById('port-img1');
-const portImg2 = document.getElementById('port-img2');
-const portImg3 = document.getElementById('port-img3');
-const block1 = document.getElementById('block-1');
-const block2 = document.getElementById('block-2');
-const block3 = document.getElementById('block-3');
+const aboutParallex = document.getElementById("about");
+const aboutParallexImg = document.getElementById("about-img");
+const portImg1 = document.getElementById("port-img1");
+const portImg2 = document.getElementById("port-img2");
+const portImg3 = document.getElementById("port-img3");
+const block1 = document.getElementById("block-1");
+const block2 = document.getElementById("block-2");
+const block3 = document.getElementById("block-3");
 const blocks = [block1, block2, block3];
 const portImg = [portImg1, portImg2, portImg3];
-
+const innerWidthMed = 840;
 
 // NAVIGATION
 
@@ -32,23 +32,24 @@ btn.addEventListener("click", function (e) {
 const obsCallBack0 = (entries) => {
   const [entry] = entries;
   if (entry.isIntersecting && entry.intersectionRatio > 0) {
-
     if (entry.boundingClientRect.y < 0) return;
-    aboutParallexImg.style.transform = 'translateY(200px)'
-    let scroll1 = window.scrollY
-    window.addEventListener('scroll', function () {
-      let value = 200 + (window.scrollY - scroll1) * -0.35;
+    aboutParallexImg.style.transform = "translateY(200px)";
+    let scroll1 = window.scrollY;
+    window.addEventListener("scroll", function () {
+      let value =
+        200 +
+        (window.scrollY - scroll1) *
+          `${window.innerWidth <= innerWidthMed ? -0.5 : -0.35}`;
       aboutParallexImg.style.transform = `translateY(${value}px)`;
-      console.log(`Outer: ${value}`);
-    })
+    });
   }
-}
+};
 
 const options0 = {
   root: null,
   threshold: 0,
-  rootMargin: '50px',
-}
+  rootMargin: "50px",
+};
 
 const observer0 = new IntersectionObserver(obsCallBack0, options0);
 observer0.observe(aboutParallex);
@@ -58,29 +59,25 @@ observer0.observe(aboutParallex);
 //   aboutParallex.style.transform = 'translateY(' + (offset * -0.37) + 'px)';
 // })
 
-
-
 // 1
 const obsCallBack1 = (entries) => {
   const [entry] = entries;
   if (entry.isIntersecting && entry.intersectionRatio > 0) {
-
     if (entry.boundingClientRect.y < 0) return;
-    portImg1.style.transform = 'translateY(200px)'
-    let scroll1 = window.scrollY
-    window.addEventListener('scroll', function () {
+    portImg1.style.transform = "translateY(200px)";
+    let scroll1 = window.scrollY;
+    window.addEventListener("scroll", function () {
       let value = 200 + (window.scrollY - scroll1) * -0.35;
       portImg1.style.transform = `translateY(${value}px)`;
-      console.log(`Outer: ${value}`);
-    })
+    });
   }
-}
+};
 
 const options1 = {
   root: null,
   threshold: 0,
-  rootMargin: '50px',
-}
+  rootMargin: "50px",
+};
 
 const observer1 = new IntersectionObserver(obsCallBack1, options1);
 observer1.observe(block1);
@@ -89,49 +86,46 @@ observer1.observe(block1);
 const obsCallBack2 = (entries) => {
   const [entry] = entries;
   if (entry.isIntersecting && entry.intersectionRatio > 0) {
-
     if (entry.boundingClientRect.y < 0) return;
-    portImg2.style.transform = 'translateY(200px)'
-    let scroll1 = window.scrollY
-    window.addEventListener('scroll', function () {
+    portImg2.style.transform = "translateY(200px)";
+    let scroll1 = window.scrollY;
+    window.addEventListener("scroll", function () {
       let value = 200 + (window.scrollY - scroll1) * -0.35;
       portImg2.style.transform = `translateY(${value}px)`;
-      console.log(`Outer: ${value}`);
-    })
+    });
   }
-}
+};
 
 const options2 = {
   root: null,
   threshold: 0,
-  rootMargin: '50px',
-}
+  rootMargin: "50px",
+};
 
 const observer2 = new IntersectionObserver(obsCallBack2, options2);
 observer2.observe(block2);
-
 
 // 3
 const obsCallBack3 = (entries) => {
   const [entry] = entries;
   if (entry.isIntersecting && entry.intersectionRatio > 0) {
-
     if (entry.boundingClientRect.y < 0) return;
-    portImg3.style.transform = 'translateY(200px)'
-    let scroll1 = window.scrollY
-    window.addEventListener('scroll', function () {
+    portImg3.style.transform = "translateY(200px)";
+    let scroll1 = window.scrollY;
+    window.addEventListener("scroll", function () {
       let value = 200 + (window.scrollY - scroll1) * -0.35;
       portImg3.style.transform = `translateY(${value}px)`;
-      console.log(`Outer: ${value}`);
-    })
+    });
   }
-}
+};
 
 const options3 = {
   root: null,
   threshold: 0,
-  rootMargin: '50px',
-}
+  rootMargin: "50px",
+};
 
 const observer3 = new IntersectionObserver(obsCallBack3, options3);
 observer3.observe(block3);
+
+console.log(window.innerWidth <= innerWidthMed ? -0.5 : -0.35);
